96c5569cede53af5dbfb53edaac1fcf1a33293ce
Mylan Li <mylanli@hotmail.com>
2024-01-18 21:46:03 +1100

    Following microsoft wpf beginner tutorial

--- PlannerTest/MainWindow.xaml.cs
+++ PlannerTest/MainWindow.xaml.cs
@@ -20,5 +20,14 @@ namespace PlannerTest
         {
             InitializeComponent();
         }
+
+        private void BtnAddName_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
+            {
+                lstNames.Items.Add(txtName.Text);
+                txtName.Clear();
+            }
+        }
     }
 }
