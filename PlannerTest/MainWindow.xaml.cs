dd4c6e4fbd4b35425cb4ca163f8c54a25314ab12
Mylan Li <mylanli@hotmail.com>
2024-01-19 23:58:19 +1100

    use trace as the debug print line; testing json; looking at global variables and scope next

--- PlannerTest/MainWindow.xaml.cs
+++ PlannerTest/MainWindow.xaml.cs
@@ -8,17 +8,29 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Text.Json;
 
 namespace PlannerTest
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
+    //test class
+    public class TestJson
+    {
+        //something about non-nullable and constructor values forcing the question mark, check after
+        public string? LineA { get; set; }
+        public string? LineB { get; set; }
+    }
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+
         }
 
         private void BtnAddName_Click(object sender, RoutedEventArgs e)
@@ -28,6 +40,23 @@ namespace PlannerTest
                 lstNames.Items.Add(txtName.Text);
                 txtName.Clear();
             }
+            Trace.WriteLine("test");
+        }
+
+        private void BtnTestPrint_Click(object sender, RoutedEventArgs e)
+        {
+            //look into accessing variables and where to put them for wpf
+            var myTest = new TestJson
+            {
+                LineA = "Hello",
+                LineB = "World",
+            };
+            string jsonString = JsonSerializer.Serialize(myTest);
+            Trace.WriteLine(jsonString);
+            myTest.LineA = txtA.Text;
+            myTest.LineB = txtB.Text;
+            jsonString = JsonSerializer.Serialize(myTest);
+            Trace.WriteLine(jsonString);
         }
     }
 }
